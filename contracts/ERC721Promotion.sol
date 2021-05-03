242a5446ae7710c8c973d38ad45486a92c73a786
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2021-05-03 11:05:30 +0700

    Lint

--- contracts/ERC721Promotion.sol
+++ contracts/ERC721Promotion.sol
@@ -4,14 +4,11 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+
 contract ERC721Promotion is ERC721, Ownable {
     constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {}
 
     address private to;
-
-    function getTo() view public returns (address){
-        return to;
-    }
 
     function setTo(address _to) external onlyOwner {
         to = _to;
@@ -23,5 +20,9 @@ contract ERC721Promotion is ERC721, Ownable {
 
     function burn(uint256 tokenId) external onlyOwner {
         _burn(tokenId);
+    }
+
+    function getTo() public view returns (address) {
+        return to;
     }
 }
